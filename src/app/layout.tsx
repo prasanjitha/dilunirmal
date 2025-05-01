12226df79e0daa338a8339d24659abc716adb556
prasanjitha <graphicdis126@.com>
2025-05-01 12:21:52 +0530

    update icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+        <head>
+  <link rel="icon" href="./icons8-wedding-day-32.png" />
+</head>
       <body
         className=""
       >
